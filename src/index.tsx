ecbd335b700d799a2820366e5e0c18302176bcb6
stelsp <stelspeh@gmail.com>
2022-08-14 16:54:50 +0300

    feat: resizible aside

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app';
 import DataProvider from './context/context';
 
+import './lib/sass/global.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   // <React.StrictMode>
